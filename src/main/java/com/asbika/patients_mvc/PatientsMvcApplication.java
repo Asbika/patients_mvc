0c11b8885f7694c0b831a09f27d6164bfacf97fd
ASBIKA HICHAM <hicham.asbika.e@gmail.com>
2022-07-31 09:15:21 +0100

    first task

---
+++ src/main/java/com/asbika/patients_mvc/PatientsMvcApplication.java
@@ -0,0 +1,13 @@
+package com.asbika.patients_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+
+}
